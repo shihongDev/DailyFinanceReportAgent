d7c1b19dd4596d562ee764af8daf9b221102db8c
shihongDev <lsh98dev@gmail.com>
2025-09-28 19:06:56 -0700

    remove tmp file

--- tmp_write_reportbuilder.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-content = []
-content.append("import { format } from 'date-fns';")
-content.append("import { GoogleGenerativeAI } from '@google/generative-ai';")
-content.append("import Logger from '../twitter/Logger.js';")
-content.append("")
